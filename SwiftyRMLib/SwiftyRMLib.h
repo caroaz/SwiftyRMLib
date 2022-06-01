e2675c16c0c8f91380647f81bf899d7534366bac
caroaz <carolinaazcurra8@gmail.com>
2022-06-01 12:22:27 -0400

    first commit

---
+++ SwiftyRMLib/SwiftyRMLib.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyRMLib.h
+//  SwiftyRMLib
+//
+//  Created by Mobdev on 01-06-22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyRMLib.
+FOUNDATION_EXPORT double SwiftyRMLibVersionNumber;
+
+//! Project version string for SwiftyRMLib.
+FOUNDATION_EXPORT const unsigned char SwiftyRMLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyRMLib/PublicHeader.h>
+
+
